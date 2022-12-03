83e9e7f14d1779cc1082bebac2ea443fd286c7b7
MarianWadia <marianwadia55@gmail.com>
2022-12-04 01:38:10 +0200

    edit in platform

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 cd www
 printenv > .env
-eb init $EB_APP --platform "Node.js 16 running on 64bit Amazon Linux 2/5.6.1" --region $AWS_DEFAULT_REGION 
+eb init $EB_APP --platform node.js --region $AWS_DEFAULT_REGION 
 eb deploy $EB_ENV
